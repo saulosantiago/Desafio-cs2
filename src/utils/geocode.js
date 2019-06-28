284be393762dc6d78d8b91c9ed18cc8043685926
Saulo Santiago <saulo8732@hotmail.com>
2019-06-28 13:27:27 -0300

    Updates

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,9 +3,13 @@ const Axios = require('axios');
 const Config = require('../../config');
 
 const location = async function getLocation(cep) {
-  await Axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${cep}&key=${Config.apiMaps}`
-  );
+  try {
+    await Axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${cep}&key=${Config.apiMaps}`
+    );
+  } catch (err) {
+    throw { message: 'Problema ao requisitar a API do Geolocation' };
+  }
   if (typeof location.data.results === 'undefined' || location.data.status === 'ZERO_RESULTS') {
     throw { message: 'CEP não encontrado!' };
   }
